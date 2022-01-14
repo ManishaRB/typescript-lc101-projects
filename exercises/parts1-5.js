2e5633f5a334fee4f843f21bcf1905af0ae8309c
Manisha B <bholemr@gmail.com>
2022-01-14 11:52:56 -0500

    Assignment completed

---
+++ exercises/parts1-5.js
@@ -0,0 +1,27 @@
+"use strict";
+exports.__esModule = true;
+// URL for the instructions: 
+// https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
+var SpaceLocation_1 = require("./SpaceLocation");
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
+var Spacecraft = /** @class */ (function () {
+    function Spacecraft(name, speedMph) {
+        this.milesPerKilometer = 0.621;
+        this.name = name;
+        this.speedMph = speedMph;
+    }
+    Spacecraft.prototype.getDaysToLocation = function (kilometersAway) {
+        var milesAway = kilometersAway * this.milesPerKilometer;
+        var hoursToMars = milesAway / this.speedMph;
+        return hoursToMars / 24;
+    };
+    Spacecraft.prototype.printDaysToLocation = function (location) {
+        console.log("".concat(this.name, " would take ").concat(this.getDaysToLocation(location.kilometersAway), " days to get to ").concat(location.name, "."));
+    };
+    return Spacecraft;
+}());
+var spaceShuttle = new Spacecraft('Determination', 17500);
+spaceShuttle.printDaysToLocation(new SpaceLocation_1.SpaceLocation('Mars', kilometersToMars));
+spaceShuttle.printDaysToLocation(new SpaceLocation_1.SpaceLocation('the Moon', kilometersToTheMoon));
+// Paste in the code from step 6 here:
